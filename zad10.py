cd42c7ee66ed668928cfe9a82fcfc836a2cd80d2
blagoyt <147176527+blagoyt@users.noreply.github.com>
2023-10-19 17:32:09 +0300

    Update zad10.py

--- zad10.py
+++ zad10.py
@@ -1,19 +1,33 @@
-bites = int(input())
-stack = []
+from collections import deque
+
+initial_bites = int(input())
+queue = deque()
+
 while True:
     command = input()
     if command == "Start":
         break
+    queue.append(command)
+
+while True:
+    command = input()
+    if command == "End":
+        break
+
+    if command.startswith("refill"):
+        command_parts = command.split()
+        command_type = command_parts[0]
+        beets = int(command_parts[1])
+        initial_bites += beets
     else:
-        stack.append(command)
-while stack:
-    person = stack.pop(0)
-    if person == "refill":
-        bites += int(input())
-    else:
-        bites -= int(input())
-        if bites >= 0:
-            print(f"{person} take bites")
+        requested_bites = int(command)
+        if not queue:
+            continue
+        person = queue.popleft()
+        if requested_bites <= initial_bites:
+            print(f"{person} take {requested_bites} bites")
+            initial_bites -= requested_bites
         else:
             print(f"{person} must wait")
-print(f"{bites} bites left")
+
+print(f"{initial_bites} bites left")

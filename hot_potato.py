9d7d0e05ecb62a21409615b89e25130e76d7a85c
100yan <133891518+StoyanHarizanov@users.noreply.github.com>
2023-10-19 18:38:33 +0300

    Update hot_potato.py
    Написал съм обяснения кое какво прави в кода на задачата

--- hot_potato.py
+++ hot_potato.py
@@ -1,13 +1,13 @@
 
-players_names = input().split(" ")
-n_throwing = int(input())
-current_player = 0
+players_names = input().split(" ")     # Четем имената на всичките играчи и го записваме в списък
+n_throwing = int(input())   #  Четем на кое хвърляне играча изгаря
+current_player = 0       #  Записваме индекса на играча който ще изгори
 while True:
-    if len(players_names) == 1:
+    if len(players_names) == 1:   #  Проверяваме дали в players_names има само 1 играч
         break
-    current_player = (n_throwing + current_player - 1) % len(players_names)
-    removed_player = players_names.pop(current_player)
-    print(f"Removed {removed_player}")
+    current_player = (n_throwing + current_player - 1) % len(players_names)   #  Изчисляваме индекса на който ще гоним играч: хвърлянето на играча който ще изгори + индекса на играча в момента - 1 защото да изчислим на кой индекс сенамира и това цялото го делим с остатък на дължината на имената на играчите
+    removed_player = players_names.pop(current_player)  #  Тук махаме от списъке играча от гония ред
+    print(f"Removed {removed_player}") 
 
 print(f"Last is {players_names[0]}")
 

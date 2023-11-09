1dbc7af06e7f2a09dfbe5884394023a5efd5b11e
blagoyt <147176527+blagoyt@users.noreply.github.com>
2023-11-09 21:57:04 +0200

    Add files via upload

---
+++ domashno3/example.py
@@ -0,0 +1,20 @@
+students_grades = {}
+n = int(input())
+for _ in range(n):
+    name, data = tuple([digit for digit in input().split()])
+    data = float(data)
+
+    if name not in students_grades:
+        students_grades[name] = []
+    students_grades[name].append(data)
+    if data > 6:
+        print("Invalid grade, please try again")
+        break
+    elif data < 2:
+        print("Invalid grade, please try again")
+        break
+
+for student, datas in sorted(students_grades.items()):
+    average_grade = sum(datas) / len(datas)
+    formatted_grades = " ".join(map(str, datas))
+    print(f"{student} -> {formatted_grades} (avg: {average_grade:.2f})")

3d9e050522048f46e854f7db7c19ba93067072f0
Georgi Dimov <147717567+dimovv4@users.noreply.github.com>
2023-11-09 21:38:54 +0200

    Update zad2_tuple.py

--- domashno3/zad2_tuple.py
+++ domashno3/zad2_tuple.py
@@ -7,8 +7,14 @@ for _ in range(n):
     if name not in students_grades:
         students_grades[name] = []
     students_grades[name].append(data)
+    if data > 6:
+        print("Invalid grade, please try again")
+        break
+    elif data < 2:
+        print("Invalid grade, please try again")
+        break
 
-for student, datas in sorted(students_grades.items()):
-    average_grade = sum(datas) / len(datas)
-    formatted_grades = " ".join(map(str, datas))
-    print(f"{student} -> {formatted_grades} (avg: {average_grade:.2f})")
+    for student, datas in sorted(students_grades.items()):
+        average_grade = sum(datas) / len(datas)
+        formatted_grades = " ".join(map(str, datas))
+        print(f"{student} -> {formatted_grades} (avg: {average_grade:.2f})")
